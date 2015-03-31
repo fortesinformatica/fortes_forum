fc5e7eb2b751a6287539c351de0fc7e4b9d5651a
Yuri Adams <yuriadams@gmail.com>
2015-03-31 11:09:52 -0300

    colocando padrão decorator dentro da engine para tentar englobar comportamentos genericos

---
+++ app/decorators/fortes_forum/forum_decorator.rb
@@ -0,0 +1,5 @@
+FortesForum::Forum.class_eval do
+  def self.get_descricao
+    "Não implementado"
+  end
+end
--- app/models/fortes_forum/forum.rb
+++ app/models/fortes_forum/forum.rb
@@ -1,7 +1,4 @@
 module FortesForum
   class Forum < ActiveRecord::Base
-    def self.get_descricao
-      "Não implementado"
-    end
   end
 end
