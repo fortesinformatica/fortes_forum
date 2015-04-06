c54d6a2a12a3025138929b7d5a0b0276c67fde54
Yuri Adams <yuriadams@gmail.com>
2015-04-06 14:42:49 -0300

    enviando @post para a view

--- app/controllers/concerns/fortes_forum/default/foruns_controller.rb
+++ app/controllers/concerns/fortes_forum/default/foruns_controller.rb
@@ -8,6 +8,7 @@ module FortesForum::Default
       skip_filter :authenticate_user!
 
       def index
+        @post = FortesForum::Post.new 
         @forum = FortesForum::Forum.get_or_create foruns_params(params)
         respond_with @forum
       end
