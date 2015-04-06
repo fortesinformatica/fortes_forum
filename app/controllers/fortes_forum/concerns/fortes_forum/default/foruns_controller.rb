9c1b56a5dd578caeab1bd2390ebfd2e83b387475
Yuri Adams <yuriadams@gmail.com>
2015-04-06 09:46:14 -0300

    colocando concern do foruns controller para modules

--- app/controllers/fortes_forum/concerns/fortes_forum/default/foruns_controller.rb
+++ app/controllers/fortes_forum/concerns/fortes_forum/default/foruns_controller.rb
@@ -1,17 +1,20 @@
-class FortesForum::Default::ForunsController < ApplicationController
-  extend ActiveSupport::Concern
+module FortesForum::Default
+  module ForunsController
+    extend ActiveSupport::Concern
 
-  respond_to :html, :json
+    included do
+      respond_to :html, :json
 
-  skip_filter :authenticate_user!
+      skip_filter :authenticate_user!
 
-  def index
-    @forum = FortesForum::Forum.get_or_create foruns_params(params)
-    respond_with @forum
-  end
+      def index
+        @forum = FortesForum::Forum.get_or_create foruns_params(params)
+        respond_with @forum
+      end
 
-  private
-  def foruns_params params
-    {id: params[:id]}
+      def foruns_params params
+        {id: params[:id]}
+      end
+    end
   end
 end
