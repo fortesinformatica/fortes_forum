e49afc3161f384c277babba7c1e872971437c053
Yuri Adams <yuriadams@gmail.com>
2015-06-25 09:22:17 -0300

    retirando debugger

--- app/models/concerns/fortes_forum/default/reply.rb
+++ app/models/concerns/fortes_forum/default/reply.rb
@@ -18,7 +18,6 @@ module FortesForum
 
         def replies_nao_sincronizados ultimo_id_sincronizado=0
           ultimo_id_sincronizado=0 if ultimo_id_sincronizado.empty?
-          debugger
           FortesForum::Reply.where(post_id: self.post_id).where("id > ?", ultimo_id_sincronizado).where(moderado: false)
         end
       end
